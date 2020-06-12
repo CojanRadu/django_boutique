3349227fb76c711b035bca21a231790cf77cb10a
CojanRadu <raduacojan@gmail.com>
2020-06-12 11:19:30 +0100

    added stripe webhooks

--- djboutiq/settings.py
+++ djboutiq/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'tai@ks#m3b!(0%=@a6np1tenfmcat3hcs&fugbbn9=m-%l&$%5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -177,3 +177,4 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
